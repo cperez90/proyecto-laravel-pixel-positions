b2f2da207c209a36d4f55cdd1bd107b49c1f75ce
riku <cperez90@alumnes.politecnicllevant.cat>
2026-01-05 18:12:28 +0000

    From Design to Blade

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        origin: 'http://192.168.1.70:8586',
+        hmr: {
+            host: '192.168.1.70',
+            clientPort: 8586,
+            path: 'hmr',
+            protocol: 'ws',
+        },
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
