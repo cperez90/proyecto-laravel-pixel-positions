19b68a427ce9253531285d8d332dbbc363dd8f01
riku <cperez90@alumnes.politecnicllevant.cat>
2026-01-05 18:27:14 +0000

    Blade and Tailwind Techniques for Your Laravel Views

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ export default {
     extend: {
         colors: {
             "black": "#060606"
+        },
+        fontFamily: {
+            "hanken-grotesk": ["Hanken Grotesk", "sans-serif"]
+        },
+        fontSize: {
+            "xxs": ".625rem"
         }
     },
   },
