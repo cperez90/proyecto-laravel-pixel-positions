9386fcbd5bcf4cc552f72de36cb861a31057cd56
riku <cperez90@alumnes.politecnicllevant.cat>
2026-01-05 19:36:43 +0000

    Jobs, Tags, TDD, Oh My!

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,16 +1,21 @@
+@props(['job'])
 <x-panel class="gap-x-6">
     <div>
         <x-employer-logo />
     </div>
-    <div class="flex-1 flex flex-col">
 
-        <a href="#" class="self-start text-sm text-gray-400">Laracast</a>
-        <h3 class="font-bold text-lg mt-3 group-hover:text-blue-600 transition-color duration-300">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
+
+        <h3 class="font-bold text-lg mt-3 group-hover:text-blue-600 transition-color duration-300">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{$job->salary}}</p>
     </div>
+
     <div>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
+
+        @foreach($job->tags as $tag)
+            <x-tag :$tag />
+        @endforeach
+
     </div>
 </x-panel>
